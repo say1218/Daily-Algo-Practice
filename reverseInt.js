28de99aff62cdda09bf33e4b403e99c223cdcdbf
smand54 <sayari.mandal@aexp.com>
2021-02-07 12:53:41 +0530

    Reverse Integers

--- reverseInt.js
+++ reverseInt.js
@@ -1,4 +1,20 @@
-function reverseInt(input) {
-	let reversed = input.toString().split("").reverse().join();
+function reverseInt1(input) {
+	let reversed = input.toString().split("").reverse().join("");
 	return parseInt(reversed) * Math.sign(input);
 }
+
+console.log(reverseInt1(-398));
+console.log(reverseInt1(706798));
+
+function reverseInt2(input) {
+	let reverse = 0;
+	let ninput = input * Math.sign(input);
+	while (ninput > 0) {
+		reverse = parseInt(reverse * 10 + (ninput % 10));
+		ninput = parseInt(ninput / 10);
+	}
+	return reverse * Math.sign(input);
+}
+
+//console.log(reverseInt2(674));
+console.log(reverseInt2(-674));
