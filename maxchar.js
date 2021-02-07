ab765816251e2ead37ffebe245a99cfb3c8911f1
smand54 <sayari.mandal@aexp.com>
2021-02-07 13:40:46 +0530

    Max Occurence of Character

---
+++ maxchar.js
@@ -0,0 +1,41 @@
+function maxChar(word) {
+	let charMap = {};
+	for (let char of word) {
+		if (charMap.hasOwnProperty(char)) {
+			charMap[char]++;
+		} else {
+			charMap[char] = 1;
+		}
+	}
+
+	// console.log(charMap);
+	// let maxChar,
+	// 	maxCharValue = 0;
+	// for (let i in charMap) {
+	// 	if (charMap[i] > maxCharValue) {
+	// 		maxCharValue = charMap[i];
+	// 		maxChar = i;
+	// 	}
+	// }
+
+	//alterbative for above
+
+	//create an array of keys
+	let keys = Object.keys(charMap);
+
+	//create an array of values
+	let values = Object.values(charMap);
+
+	//get index of max value
+	let maxIndex = values.indexOf(Math.max(...values));
+
+	//locate the key for the correspondong value
+	maxChar = keys[maxIndex];
+
+	return maxChar;
+}
+
+console.log(maxChar("Mpissisippppppiiii"));
+
+//for..in ---> key (objects,and everything else to get key)
+//for..of ----> values (strings,arrays,maps etc)
