a066ed755387e195868ff42d84c18b6ae5933ee1
sayari mandal <sayarimandal1218@gmail.com>
2021-05-13 20:10:20 +0530

    resolution handler is resolved when handler is called

--- JS/promise.js
+++ JS/promise.js
@@ -1,5 +1,29 @@
 class MyPromise {
 	constructor(executor) {
-		executor();
+		console.log("her in cons");
+		this._resolutionQueue = [];
+		executor(this._resolve.bind(this));
+	}
+	//private method
+	_resolve(value) {
+		while (this._resolutionQueue.length > 0) {
+			var handler = this._resolutionQueue.shift();
+			handler(value);
+		}
+	}
+
+	then(resolutionHandler) {
+		console.log("in then");
+		this._resolutionQueue.push(resolutionHandler);
 	}
 }
+
+var promise = new MyPromise(function (resolve) {
+	setTimeout(function () {
+		resolve("hello");
+	}, 100);
+});
+
+promise.then(function (res) {
+	console.log(res);
+});
